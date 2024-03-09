bbf06e212d84992a001219501addfeb11fb2583f
EnmmmmOvO <wangjinghan0218@gmail.com>
2024-03-09 15:31:41 +1100

    init code, draw struct finish

---
+++ src/draw.rs
@@ -0,0 +1,99 @@
+use unsvg::*;
+
+pub struct Draw<'a> {
+    x: f32,
+    y: f32,
+    draw: bool,
+    color: Color,
+    direction: i32,
+    image: &'a mut Image,
+}
+
+impl<'a> Draw<'a> {
+    pub fn new(x: f32, y: f32, image: &'a mut Image) -> Self {
+        Self {
+            x: x / 2.0,
+            y: y / 2.0,
+            draw: false,
+            color: COLORS[7],
+            direction: 0,
+            image
+        }
+    }
+
+    pub fn x(&self) -> f32 {
+        self.x
+    }
+
+    pub fn y(&self) -> f32 {
+        self.y
+    }
+
+    pub fn color(&self) -> usize {
+        COLORS.iter().position(|&c| c == self.color).unwrap()
+    }
+
+    pub fn direction(&self) -> i32 {
+        self.direction
+    }
+
+    pub fn draw(&self) -> bool {
+        self.draw
+    }
+
+    pub fn pen_up(&mut self) {
+        self.draw = false;
+    }
+
+    pub fn pen_down(&mut self) {
+        self.draw = true;
+    }
+
+    pub fn pen_move(&mut self, direction: i32, mut distance: f32) {
+        let mut dir = self.direction + direction;
+
+        if distance < 0f32 {
+            dir += 180;
+            distance = distance.abs();
+        }
+
+        if !self.draw {
+            let temp = get_end_coordinates(self.x, self.y, dir, distance);
+            self.x = temp.0;
+            self.y = temp.1;
+        } else {
+            match self.image.draw_simple_line(self.x, self.y, dir, distance, self.color) {
+                Ok(result) => {
+                    self.x = result.0;
+                    self.y = result.1;
+                }
+                Err(e) => eprintln!("Error drawing line: {e}"),
+            }
+        }
+    }
+
+    pub fn set_pen_color(&mut self, index: usize) {
+        self.color = COLORS[index];
+    }
+
+    pub fn turn(&mut self, angle: i32) {
+        self.direction += angle;
+    }
+
+    pub fn set_heading(&mut self, angle: i32) {
+        self.direction = angle;
+    }
+
+    pub fn set_x(&mut self, x: f32) {
+        self.x = x;
+    }
+
+    pub fn set_y(&mut self, y: f32) {
+        self.y = y;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+}
---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use clap::Parser;
+use unsvg::Image;
+use draw::pen;
+
+pub mod draw;
+mod compiler;
+
+/// A simple program to parse four arguments using clap.
+#[derive(Parser)]
+struct Args {
+    /// Path to a file
+    file_path: std::path::PathBuf,
+
+    /// Path to an svg or png image
+    image_path: std::path::PathBuf,
+
+    /// Height
+    height: u32,
+
+    /// Width
+    width: u32,
+}
+
+fn main() -> Result<(), ()> {
+    let args: Args = Args::parse();
+
+    // Access the parsed arguments
+    let file_path = args.file_path;
+    let image_path = args.image_path;
+    let height = args.height;
+    let width = args.width;
+
+    let mut image = Image::new(width, height);
+    let mut pen = pen::new(width as f32, height as f32, &mut image);
+
+
+    match image_path.extension().map(|s| s.to_str()).flatten() {
+        Some("svg") => {
+            let res = image.save_svg(&image_path);
+            if let Err(e) = res {
+                eprintln!("Error saving svg: {e}");
+                return Err(());
+            }
+        }
+        Some("png") => {
+            let res = image.save_png(&image_path);
+            if let Err(e) = res {
+                eprintln!("Error saving png: {e}");
+                return Err(());
+            }
+        }
+        _ => {
+            eprintln!("File extension not supported");
+            return Err(());
+        }
+    }
+
+    Ok(())
+}
